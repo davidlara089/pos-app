6a5614c67aa1f7422b6e3e85446f2eae6e8ee166
davidlara089 <148598977+davidlara089@users.noreply.github.com>
2025-05-20 13:26:26 -0600

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'https://softwarekids.cleverapps.io/';  // Tu dominio completo con https://
+$config['base_url'] = $_ENV['BASE_URL'];
 /*
 |--------------------------------------------------------------------------
 | Index File
